cec49366c014e365a385a9d868e58dd015188045
Tyler Ferguson <tyler.ferguson@ndm.ox.ac.uk>
2020-05-19 23:55:35 +0100

    Include historical active franchise team names in data/TEAM_IDS

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' Basketball Reference IDs for Active NBA teams.
 #'
 #' A dataset containing the franchise names and IDs used by Basketball Reference to
-#' uniquely identify each active team.
+#' uniquely identify each active team and its historical names.
 #'
-#' @format A data frame with 30 rows and 2 variables:
+#' @format A data frame with 50 rows and 2 variables:
 #' \describe{
 #'   \item{Franchise}{franchise name}
 #'   \item{TeamId}{ID of team used in Basketball Reference URL}
--- data-raw/TEAM_IDS.R
+++ data-raw/TEAM_IDS.R
@@ -1,4 +1,4 @@
-TEAM_IDS <- teams.team_index() %>%
+TEAM_IDS <- teams.team_index(include_total_history = TRUE) %>%
   select(Franchise, TeamId)
 
 usethis::use_data(TEAM_IDS, overwrite = TRUE)
